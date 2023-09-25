e376348b84c9c7364c194010c6f77cf7ac790d94
smeikol <564500@edu.rocmn.nl>
2023-09-25 12:33:33 +0200

    challas maat geeft coole layout

--- Pages/Home/index.php
+++ Pages/Home/index.php
@@ -1,22 +1,63 @@
-<?php
-
-
-
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../Assets/CSS/Main.css">
-    <title>Home</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ROCMN Stemwijzer</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f4f4f4;
+      color: #333;
+      padding: 20px;
+      text-align: center;
+    }
+
+    .header {
+      background-color: #007f91;
+      color: white;
+      padding: 20px;
+    }
+
+    .logo {
+      width: 100px;
+      height: 100px;
+    }
+
+    .button {
+      padding: 10px 20px;
+      font-size: 18px;
+      background-color: #007f91;
+      color: white;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      margin-top: 20px;
+    }
+
+    .button:hover {
+      background-color: #005f6b;
+    }
+  </style>
 </head>
 
 <body>
-    <p>Dit is de intro text voor de Stemwijzer</p>
-    <button>Start de Stemwijzer</button>
+  <div class="header">
+    <img src="link-naar-het-ROC-logo" alt="ROC Logo" class="logo">
+    <h1>Welkom bij de ROCMN Stemwijzer</h1>
+  </div>
+
+  <div>
+    <h2>Ontdek welke politieke partij het beste bij jou past!</h2>
+    <p>Waar staan de politieke partijen voor?</p>
+    <p>Krijg inzicht in de standpunten van verschillende politieke partijen op basis van belangrijke thema's.</p>
+
+    <h3>Hoe werkt het?</h3>
+    <p>Beantwoord een aantal vragen over actuele onderwerpen. Op basis van jouw antwoorden krijg je een overzicht van de politieke partijen die het beste aansluiten bij jouw standpunten.</p>
+
+    <a href="link-naar-stemwijzer" class="button">Start de Stemwijzer</a>
+  </div>
 </body>
 
 </html>
