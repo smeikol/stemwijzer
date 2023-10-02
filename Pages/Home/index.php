7604ebaba438d2aec8025816d60bbfb6a1981904
rickbestgamer <95346478+rickbestgamer@users.noreply.github.com>
2023-10-02 13:19:30 +0200

    je moeder

--- Pages/Home/index.php
+++ Pages/Home/index.php
@@ -5,55 +5,16 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ROCMN Stemwijzer</title>
-  <style>
-    :root {
-      --Main-color: #007ac9;
-      --Secundair-color: #2E3192;
-    }
-
-    body {
-      font-family: Arial, sans-serif;
-      background-color: #f4f4f4;
-      color: #333;
-      padding: 20px;
-      text-align: center;
-    }
-
-    .header {
-      background-color: var(--Main-color);
-      color: white;
-      padding: 20px;
-    }
-
-    .logo {
-      width: 100px;
-      height: 100px;
-    }
-
-    .button {
-      padding: 10px 20px;
-      font-size: 18px;
-      background-color: var(--Main-color);
-      color: white;
-      border: none;
-      border-radius: 5px;
-      cursor: pointer;
-      margin-top: 20px;
-    }
-
-    .button:hover {
-      background-color: var(--Secundair-color);
-    }
-  </style>
+  <link rel="stylesheet" href="../../Assets/CSS/Main.css">
+  <link rel="stylesheet" href="../../Assets/CSS/Home.css">
 </head>
 
 <body>
   <div class="header">
-    <img src="https://www.rocmn.nl/themes/custom/rocmn/logo.svg?v=1693382188" alt="ROC Logo" class="logo">
     <h1>Welkom bij de ROCMN Stemwijzer</h1>
   </div>
 
-  <div>
+  <div class="Content">
     <h2>Ontdek welke politieke partij het beste bij jou past!</h2>
     <p>Waar staan de politieke partijen voor?</p>
     <p>Krijg inzicht in de standpunten van verschillende politieke partijen op basis van belangrijke thema's.</p>
@@ -61,7 +22,7 @@
     <h3>Hoe werkt het?</h3>
     <p>Beantwoord een aantal vragen over actuele onderwerpen. Op basis van jouw antwoorden krijg je een overzicht van de politieke partijen die het beste aansluiten bij jouw standpunten.</p>
 
-    <a href="../Stemwijzer/" class="button">Start de Stemwijzer</a>
+    <a href="../Stemwijzer/" class="button"><button>Start de Stemwijzer</button></a>
   </div>
 </body>
 
