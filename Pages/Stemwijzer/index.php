72ba454d61209c19061a1058b12dc8cafe70aa1e
rickbestgamer <95346478+rickbestgamer@users.noreply.github.com>
2023-10-06 11:44:15 +0200

    shut

--- Pages/Stemwijzer/index.php
+++ Pages/Stemwijzer/index.php
@@ -9,13 +9,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../Assets/CSS/Main.css">
+    <link rel="stylesheet" href="../../Assets/CSS/Stemwijzer.css">
     <title>Stemwijzer</title>
 </head>
 
 <body>
     <div class="QuestionWrapper">
-        <p id="Question"></p>
-        
+        <p id="QuestionHeader"></p>
+
     </div>
 </body>
 
