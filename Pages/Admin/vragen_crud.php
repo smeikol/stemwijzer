dee093222b4cf24b4f6ecc51cce056cc4119cb5c
Rayanely <562891@edu.rocmn.nl>
2023-10-26 09:44:45 +0200

    Crud voor de vragen

---
+++ Pages/Admin/vragen_crud.php
@@ -0,0 +1,19 @@
+<?php
+include_once "../../Assets/Templates/Conn.php";
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD | Vragen</title>
+    <link rel="stylesheet" href="../../Assets/CSS/Main.css">
+    <link rel="stylesheet" href="../../Assets/CSS/Home.css">
+    <link rel="stylesheet" href="../../Assets/CSS/admin.css">   
+</head>
+<body>
+    <div class="header">
+    <h1>ROCMN Stemwijzer Admin</h1>
+    </div>
+</body>
+</html>
