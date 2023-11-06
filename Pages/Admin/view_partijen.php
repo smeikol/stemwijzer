692a41844c0d834df7ddef84e7af935872a6cbb1
ThijsHer <96177178+ThijsHer@users.noreply.github.com>
2023-11-06 15:03:28 +0100

    delete vragen werkt half op view_partijen :sku

--- Pages/Admin/view_partijen.php
+++ Pages/Admin/view_partijen.php
@@ -41,82 +41,87 @@ if (isset($_POST['toevoegen'])) {
     header("Refresh:0");
 }
 
-    $sql = "SELECT * FROM partij";
-    $result = $CONN->query($sql);
-    ?>
+$sql = "SELECT * FROM partij";
+$result = $CONN->query($sql);
+?>
 
-    <!-- Create - Form to add a new party -->
-    <h2>Partij toevoegen</h2>
+<!-- Create - Form to add a new party -->
+<h2>Partij toevoegen</h2>
+<form method="post">
+    <label><a class="text-white">Naam:</a><input type="text" name="naam"></label>
+    <input class="action_a toevoeg_button" type="submit" name="toevoegen" value="Toevoegen">
+</form>
+
+<!-- Read - List of parties in a table -->
+<h2>Partijen</h2>
+<?php
+if ($result->num_rows > 0) {
+    echo '<table>';
+    echo '<tr><th>Naam</th><th>Actie</th></tr>';
+    while ($row = $result->fetch_assoc()) {
+        echo '<tr>';
+        echo '<td>' . $row['naam'] . '</td>';
+        echo '<td><a class="action_del margin-button" href="?verwijderen=' . $row['partij_id'] . '">Verwijder</a><a class="action_a" href="?bijwerken=' . $row['partij_id'] . '">Updaten</a><a class="action_a" href="partij_vragen.php?partij_id=' . $row['partij_id'] . '">Bekijken</a></td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+} else {
+    echo "<a class=\"text_white\">Geen partijen gevonden.</a>";
+}
+?>
+
+
+<!-- Update - Form to update party name -->
+<?php
+if (isset($_GET['bijwerken'])) {
+    $partij_id = $_GET['bijwerken'];
+    $sql = "SELECT * FROM partij WHERE partij_id=?";
+    $STMT = $CONN->prepare($sql);
+    $STMT->bind_param("s", $partij_id);
+    $STMT->execute();
+    $result = $STMT->get_result();
+    $row = $result->fetch_assoc();
+    ?>
+    <h2>Werk de naam van de partij bij</h2>
     <form method="post">
-        <label><a class="text-white">Naam:</a><input type="text" name="naam"></label>
-        <input class="action_a toevoeg_button" type="submit" name="toevoegen" value="Toevoegen">
+        <input type="hidden" name="partij_id" value="<?php echo $row['partij_id']; ?>">
+        <label><a class="text-white">Nieuwe naam:</a> <input type="text" name="nieuwe_naam" value="<?php echo $row['naam']; ?>"></label>
+        <input class="action_a toevoeg_button" type="submit" name="bijwerken" value="Bijwerken">
     </form>
-
-    <!-- Read - List of parties in a table -->
-    <h2>Partijen</h2>
     <?php
-    if ($result->num_rows > 0) {
-        echo '<table>';
-        echo '<tr><th>Naam</th><th>Actie</th></tr>';
-        while ($row = $result->fetch_assoc()) {
-            echo '<tr>';
-            echo '<td>' . $row['naam'] . '</td>';
-            echo '<td><a class="action_del margin-button" href="?verwijderen=' . $row['partij_id'] . '">Verwijder</a><a class="action_a" href="?bijwerken=' . $row['partij_id'] . '">Updaten</a><a class="action_a" href="partij_vragen.php?partij_id=' . $row['partij_id'] . '">Bekijken</a></td>';
-            echo '</tr>';
-        }
-        echo '</table>';
-    } else {
-        echo "<a class=\"text_white\">Geen partijen gevonden.</a>";
-    }
-    ?>
+}
 
+// Update - Handle the party name update
+if (isset($_POST['bijwerken'])) {
+    $partij_id = $_POST['partij_id'];
+    $nieuwe_naam = $_POST['nieuwe_naam'];
 
-    <!-- Update - Form to update party name -->
-    <?php
-    if (isset($_GET['bijwerken'])) {
-        $partij_id = $_GET['bijwerken'];
-        $sql = "SELECT * FROM partij WHERE partij_id=?";
-        $STMT = $CONN->prepare($sql);
-        $STMT->bind_param("s", $partij_id);
-        $STMT->execute();
-        $result = $STMT->get_result();
-        $row = $result->fetch_assoc();
-    ?>
-        <h2>Werk de naam van de partij bij</h2>
-        <form method="post">
-            <input type="hidden" name="partij_id" value="<?php echo $row['partij_id']; ?>">
-            <label><a class="text-white">Nieuwe naam:</a> <input type="text" name="nieuwe_naam" value="<?php echo $row['naam']; ?>"></label>
-            <input class="action_a toevoeg_button" type="submit" name="bijwerken" value="Bijwerken">
-        </form>
-    <?php
-    }
+    $sql = "UPDATE partij SET naam=? WHERE partij_id=?";
 
-    // Update - Handle the party name update
-    if (isset($_POST['bijwerken'])) {
-        $partij_id = $_POST['partij_id'];
-        $nieuwe_naam = $_POST['nieuwe_naam'];
+    $STMT = $CONN->prepare($sql);
+    $STMT->bind_param("ss", $nieuwe_naam, $partij_id);
+    $STMT->execute();
+    $result = $STMT->get_result();
 
-        $sql = "UPDATE partij SET naam=? WHERE partij_id=?";
+    header("Refresh:0");
+}
 
-        $STMT = $CONN->prepare($sql);
-        $STMT->bind_param("ss", $nieuwe_naam, $partij_id);
-        $STMT->execute();
-        $result = $STMT->get_result();
+// Delete - Remove party from the database
+if (isset($_GET['verwijderen'])) {
+    $partij_id = $_GET['verwijderen'];
 
-        header("Refresh:0");
-    }
+    $delete_antwoord_sql = "DELETE FROM partij_antwoord WHERE partij_id=?";
+    $delete_antwoord_stmt = $CONN->prepare($delete_antwoord_sql);
+    $delete_antwoord_stmt->bind_param("s", $partij_id);
+    $delete_antwoord_stmt->execute();
 
-    // Delete - Remove party from the database
-    if (isset($_GET['verwijderen'])) {
-        $partij_id = $_GET['verwijderen'];
-        $sql = "DELETE FROM partij WHERE partij_id=?";
-        $STMT = $CONN->prepare($sql);
-        $STMT->bind_param("s", $partij_id);
-        $STMT->execute();
-        $result = $STMT->get_result();
-        header("Location: view_partijen.php");
-    }
-    ?>
+    $delete_partij_sql = "DELETE FROM partij WHERE partij_id=?";
+    $delete_partij_stmt = $CONN->prepare($delete_partij_sql);
+    $delete_partij_stmt->bind_param("s", $partij_id);
+    $delete_partij_stmt->execute();
+
+    header("Refresh:1");}
+
+?>
 </body>
-
 </html>
