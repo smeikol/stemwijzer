5435a64f09a882e22bf51bded8267893ddbd8174
smeikol <564500@edu.rocmn.nl>
2023-11-06 15:34:11 +0100

    oepsie heb het perongeluk weer gedaan

--- Pages/Admin/vraag_del.php
+++ Pages/Admin/vraag_del.php
@@ -10,6 +10,13 @@ $result = $CONN->query($sql);
 
 if (isset($_GET['vraag_id'])) {
     $id = $_GET['vraag_id'];
+
+
+    $delete_antwoord_sql = "DELETE FROM partij_antwoord WHERE vraag_id=?";
+    $delete_antwoord_stmt = $CONN->prepare($delete_antwoord_sql);
+    $delete_antwoord_stmt->bind_param("s", $id);
+    $delete_antwoord_stmt->execute();
+
     $sql = "DELETE FROM vraag WHERE vraag_id = ?";
     $STMT = $CONN->prepare($sql);
     $STMT->bind_param("s", $id);
